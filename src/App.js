dfb7002f129d20bf8dc9b959da293c836ec8eb56
nikhilarya <nikhilarya31@gmail.com>
2018-08-18 21:42:04 +0530

    project added

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Clarifai from 'clarifai';
+//import Particles from 'react-particles-js';
+
+//import logo from './logo.svg';
+import './App.css';
+
+const app = new Clarifai.App({
+ apiKey: '56f0cd0c34e1482ab0b1361647fd73b1'
+});
+
+class App extends Component {
+  constructor(){
+    super();
+  
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementByID('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+    .then(response => console.log(this.displayFaceBox(this.calculateFaceLocation(response)))
+    .catch(err => console.log(err))
+    );
+  }
+
+  render() {
+    return (
+      <div className="App">
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+        {/*
+                  <Particles 
+              className='particles'
+              params={particlesOptions}
+        />
+                
+                */}
+      </div>
+    );
+  }
+}
+
+export default App;
